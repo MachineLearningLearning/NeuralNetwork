28ecc8cdae35f960eb1a6ed0f52c6db39e990ffd
Mark Jacobsen <mark.jacobsen2003@gmail.com>
2022-03-01 14:50:30 +0000

    small changes to skeleton code

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,9 +2,7 @@
 This file contains the NeuralNetwork class
 
 Parameters:
-    input_nodes: the amount of input nodes as integer
-    middle_layers: list of integers, where the integers represent amount of nodes per layer
-    output_nodes: the amount of output nodes
+    layers: list of integers, where the integers represent amount of nodes per layer
 
 *Further explanation of class etc.*
 *Also feel free to change the parameters if you think its better in some other way*
@@ -15,16 +13,17 @@ import numpy as np
 
 
 class NeuralNetwork:
-    def __init__(self, input_nodes, middle_layers, output_nodes):
-        self.input_nodes = input_nodes
-        self.middle_layers = middle_layers
-        self.output_nodes = output_nodes
+    def __init__(self, layers):
+        # setup
+        self.layers = layers
+
+        # generate weights and biases
 
     def feed_forward(self, inputs):
         """
         feed through the network and generate outputs
         for specific inputs
-        :param inputs: list of values for the input layer nodes
+        :param inputs: numpy array of values for the input layer nodes
         :return: list of values each representing one output from an output node
         """
         # I (Mark Jacobsen) will work on this...
