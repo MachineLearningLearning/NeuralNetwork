fabd6d22ecd7a74ec8e775865d9cb0b0c2cfd7db
Mark Jacobsen <mark.jacobsen2003@gmail.com>
2022-03-26 17:03:51 +0000

    added spacing between nodes in visualization

--- Visualizer/NetworkVisualizer.py
+++ Visualizer/NetworkVisualizer.py
@@ -24,12 +24,17 @@ class NetworkVisualizer:
         :return: list of lists for each layer in network
         """
         nodes = []
+        # TODO need to do the x,y maths here for better visualization
+        current_x = 100
         for layer in self.network.layers:
+            current_y = 100
             node_layer = []
             for _ in range(layer):
-                n = Node.Node(10, 10, 10)
+                n = Node.Node(current_x, current_y, 20)
                 node_layer.append(n)
+                current_y += 100
             nodes.append(node_layer)
+            current_x += 100
         return nodes
 
     def draw(self, screen):
