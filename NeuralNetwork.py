4ac248d8660bc643a4a36035e599111a344d9c3a
Will Gould <will.gould2003@outlook.com>
2022-03-29 20:00:29 +0100

    Remove old comments

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -58,27 +58,11 @@ class NeuralNetwork:
         """
 
         network_values = self.feed_forward(np.transpose(inputs))
-
-
         '''
         errors where [0] is output of the nn and [-1] is first hidden layer
         gradients where [0] is output of nn and [-1] is first hidden layer
         '''
         node_errors, node_gradients = self.backpropagation(network_values, expected_outputs)
-
-        '''
-
-        output_delta_bias = learning_rate * node_errors[0] * node_gradients[0]
-        output_delta_weight = np.dot(output_delta_bias, np.transpose(network_values[-2]))
-
-        self.biases[-1] = self.biases[-1] + output_delta_bias
-        self.weights[-1] = self.weights[-1] + output_delta_weight
-
-        hidden2_delta_bias = learning_rate * node_errors[1] * node_gradients[1]
-        hidden2_delta_weight = np.dot(hidden2_delta_bias, np.transpose(network_values[-3]))
-
-        '''
-
 
         for i in range(0, len(node_errors) - 1):
             delta_bias = learning_rate * node_errors[i] * node_gradients[i]
@@ -87,52 +71,12 @@ class NeuralNetwork:
             self.biases[-i - 1] = self.biases[-i - 1] + delta_bias
             self.weights[-i - 1] = self.weights[-i - 1] + delta_weights
 
-        #last to change is the weights from the input nodes
-
+        #last to change is the weights from the input nodes to the first hidden layer
         delta_bias = learning_rate * node_errors[-1] * node_gradients[-1]
         delta_weights = np.dot(delta_bias, inputs)
 
         self.biases[0] = self.biases[0] + delta_bias
-        self.weights[0] = self.weights[0] + delta_weights
-
-        '''
-        for i in range(len(network_values) - 2, -1, -1):
-            layer_values = network_values[i]
-            layer_errors = np.dot(np.transpose(self.weights[i + 1]), errors[-1])
-            layer_gradients = [value * (1 - value) for value in layer_values]
-            delta_biases = learning_rate * errors[-1] * layer_gradients #gradients[-1]
-            delta_weights = np.dot(delta_biases, np.transpose(layer_values))
-
-            self.weights[i + 1] = np.add(self.weights[i + 1], delta_weights)
-            self.biases[i] = np.add(self.biases[i], delta_biases)
-
-            errors.append(layer_errors)
-            gradients.append(layer_gradients)
-
-        hidden_values = network_values[0]
-        hidden_errors = np.dot(np.transpose(self.weights[-1]), output_errors)
-        hidden_gradients = [hidden_value * (1 - hidden_value) for hidden_value in hidden_values]
-
-        delta_hidden_biases = learning_rate * np.dot(output_errors, output_gradients)
-        delta_start_biases = learning_rate * hidden_errors * hidden_gradients
-
-        #np.transpose(outputs) or np.transpose(hidden_layer_values)
-        delta_hidden = np.dot(delta_hidden_biases, np.transpose(network_outputs))
-        
-        delta_start = np.dot(delta_start_biases, np.transpose(hidden_values))
-        '''
-        '''
-
-        delta_start_biases = learning_rate * errors[-1] * gradients[-1]
-        delta_start = np.dot(delta_start_biases, np.transpose(network_values[0]))
-        self.weights[0] = np.add(self.weights[0], delta_start)
-
-        #apply changes
-        #print(self.weights)
-        #self.weights[1] = np.add(self.weights[1], delta_hidden)
-        #self.biases[1] = np.add(self.biases[1], delta_hidden_biases)
-
-        '''      
+        self.weights[0] = self.weights[0] + delta_weights  
 
     def backpropagation(self, network_values, expected_outputs):
         errors = [np.transpose(np.subtract(expected_outputs, np.transpose(network_values[-1])))]
@@ -150,7 +94,7 @@ class NeuralNetwork:
     """
 
 if __name__ == "__main__":
-    network = NeuralNetwork([2, 2, 3, 1])
+    network = NeuralNetwork([2, 2, 1])
     print(network.feed_forward(np.transpose(np.array([[0, 0]])))[-1])
     print(network.feed_forward(np.transpose(np.array([[1, 0]])))[-1])
     print(network.feed_forward(np.transpose(np.array([[0, 1]])))[-1])
