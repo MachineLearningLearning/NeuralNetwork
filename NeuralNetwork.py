0d5b767ffcd37826614c85ea8ea70f06ce3cc02d
Will Gould <will.gould2003@outlook.com>
2022-03-29 17:51:20 +0100

    Finsh backprop and test function

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -9,7 +9,6 @@ Parameters:
 
 contributors: Mark Jacobsen, Will Gould, *add your name here...*
 """
-from distutils.log import error
 import numpy as np
 import random
 
@@ -51,7 +50,7 @@ class NeuralNetwork:
             outputs.append(inputs)
         return outputs
 
-    def train(self, inputs, expected_ouputs, learning_rate):
+    def train(self, inputs, expected_outputs, learning_rate):
         """
         :param inputs: numpy array of values for the input layer nodes
         :param expected_outputs: numpy array of values that are what the output layer nodes should be
@@ -60,23 +59,47 @@ class NeuralNetwork:
 
         network_values = self.feed_forward(np.transpose(inputs))
 
-        network_outputs = network_values[-1]
-        output_errors = np.transpose(expected_ouputs) - network_outputs
-        output_gradients = [output * (1 - output) for output in network_outputs]
 
-        output_delta_bias = learning_rate * output_errors * output_gradients
-        self.biases[-1] = np.add(self.biases[-1], output_delta_bias)
+        '''
+        errors where [0] is output of the nn and [-1] is first hidden layer
+        gradients where [0] is output of nn and [-1] is first hidden layer
+        '''
+        node_errors, node_gradients = self.backpropagation(network_values, expected_outputs)
 
-        errors = [output_errors]
-        gradients = [output_gradients]
+        '''
 
+        output_delta_bias = learning_rate * node_errors[0] * node_gradients[0]
+        output_delta_weight = np.dot(output_delta_bias, np.transpose(network_values[-2]))
+
+        self.biases[-1] = self.biases[-1] + output_delta_bias
+        self.weights[-1] = self.weights[-1] + output_delta_weight
+
+        hidden2_delta_bias = learning_rate * node_errors[1] * node_gradients[1]
+        hidden2_delta_weight = np.dot(hidden2_delta_bias, np.transpose(network_values[-3]))
+
+        '''
+
+
+        for i in range(0, len(node_errors) - 1):
+            delta_bias = learning_rate * node_errors[i] * node_gradients[i]
+            delta_weights = np.dot(delta_bias, np.transpose(network_values[-2 - i]))
+
+            self.biases[-i - 1] = self.biases[-i - 1] + delta_bias
+            self.weights[-i - 1] = self.weights[-i - 1] + delta_weights
+
+        #last to change is the weights from the input nodes
+
+        delta_bias = learning_rate * node_errors[-1] * node_gradients[-1]
+        delta_weights = np.dot(delta_bias, inputs)
+
+        self.biases[0] = self.biases[0] + delta_bias
+        self.weights[0] = self.weights[0] + delta_weights
+
+        '''
         for i in range(len(network_values) - 2, -1, -1):
-            print(i)
             layer_values = network_values[i]
             layer_errors = np.dot(np.transpose(self.weights[i + 1]), errors[-1])
             layer_gradients = [value * (1 - value) for value in layer_values]
-            print(errors[-1])
-            print(layer_gradients)
             delta_biases = learning_rate * errors[-1] * layer_gradients #gradients[-1]
             delta_weights = np.dot(delta_biases, np.transpose(layer_values))
 
@@ -85,9 +108,7 @@ class NeuralNetwork:
 
             errors.append(layer_errors)
             gradients.append(layer_gradients)
-        print("out")
 
-        '''
         hidden_values = network_values[0]
         hidden_errors = np.dot(np.transpose(self.weights[-1]), output_errors)
         hidden_gradients = [hidden_value * (1 - hidden_value) for hidden_value in hidden_values]
@@ -100,6 +121,7 @@ class NeuralNetwork:
         
         delta_start = np.dot(delta_start_biases, np.transpose(hidden_values))
         '''
+        '''
 
         delta_start_biases = learning_rate * errors[-1] * gradients[-1]
         delta_start = np.dot(delta_start_biases, np.transpose(network_values[0]))
@@ -110,12 +132,18 @@ class NeuralNetwork:
         #self.weights[1] = np.add(self.weights[1], delta_hidden)
         #self.biases[1] = np.add(self.biases[1], delta_hidden_biases)
 
-        
-        
+        '''      
 
-    def backpropagation(self):
-        # someone claim this??
-        return
+    def backpropagation(self, network_values, expected_outputs):
+        errors = [np.transpose(np.subtract(expected_outputs, np.transpose(network_values[-1])))]
+        gradients = [[output * (1 - output) for output in network_values[-1]]]
+
+        for i in range(len(network_values) - 1, 0, -1):
+            errors.append(np.dot(np.transpose(self.weights[i]), errors[-1]))
+            gradients.append([value * (1 - value) for value in network_values[i - 1]])
+
+
+        return errors, gradients
 
     """
     other stuff...
@@ -127,9 +155,8 @@ if __name__ == "__main__":
     print(network.feed_forward(np.transpose(np.array([[1, 0]])))[-1])
     print(network.feed_forward(np.transpose(np.array([[0, 1]])))[-1])
     print(network.feed_forward(np.transpose(np.array([[1, 1]])))[-1])
-    print()
     inputs = [[[[1, 0]], [1]], [[[0, 1]], [1]], [[[1, 1]], [1]], [[[0, 0]], [0]]]
-    for i in range(0, 1):
+    for i in range(0, 10000):
         test_data = inputs[random.randint(0, 3)]
         network.train(np.array(test_data[0]), np.array(test_data[1]), 0.1)
     print(network.feed_forward(np.transpose(np.array([[0, 0]])))[-1])
